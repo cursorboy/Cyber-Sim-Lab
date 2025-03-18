92fead02650f5d0a962adfe13fadafdbd018c2ef
Piam <piamparekh17@gmail.com>
2025-03-18 13:29:46 -0700

    just doing stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -18,5 +17,5 @@ const nextConfig = {
   },
 }
 
-export default nextConfig;
+export default nextConfig
 
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,7 +2,10 @@
 const config = {
   plugins: {
     tailwindcss: {},
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
   },
 };
 
 export default config;
+
